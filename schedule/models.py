1b6dc968af7a815f9958947b8457f8780b0d279b
mat_mal <matvey_maljanov@rambler.ru>
2024-06-15 19:26:10 +0300

    models redone

--- schedule/models.py
+++ schedule/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class Route(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='Название маршрута')
+    start_point = models.CharField(max_length=20, verbose_name='Начальная станция')
+    end_point = models.CharField(max_length=20, verbose_name='Конечная станция')
 
     def __str__(self):
         return self.name
@@ -10,8 +12,9 @@ class Route(models.Model):
 
 class Schedule(models.Model):
     name = models.ForeignKey(Route, on_delete=models.CASCADE)
-    departure_time = models.TimeField()
-    arrival_time = models.TimeField()
+    departure_time = models.TimeField(verbose_name='Начало движения')
+    arrival_time = models.TimeField(verbose_name='Окончание движения')
+    interval = models.IntegerField(verbose_name='Интервал движения')
 
     def __str__(self):
         return f'{self.name} - {self.departure_time} to {self.arrival_time}'
