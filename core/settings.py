b195e5014e32626edcd6d8522c5219dd5b26f5b4
mat_mal <matvey_maljanov@rambler.ru>
2024-06-17 08:29:01 +0300

    header

--- core/settings.py
+++ core/settings.py
@@ -91,4 +91,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+
+# Настройки медиа-файлов
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
